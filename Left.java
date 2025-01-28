680dba3ff81e9b3950bc65f83986a3b509281d37
KASTURI SRAVYA <kasturisravya15@gmail.com>
2025-01-28 16:08:13 +0530

    Add files via upload

---
+++ Left.java
@@ -0,0 +1,9 @@
+public class Left{
+    public static void main(String[] args) {
+        int num = 5; // Binary representation: 101
+        int shiftedNum = num << 1; // Shift left by 1 bit
+        System.out.println("Original Number: " + num);
+        System.out.println("Shifted Number: " + shiftedNum);
+    }
+}
+
